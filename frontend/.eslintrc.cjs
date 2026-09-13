f583d49b61cb1c1ece03f2ff5ce624fdc3da285f
NatsuDrag9 <rohitimandi9@zoho.com>
2026-09-13 08:21:32 +0300

    feat(fe-lint): configure ESLint Airbnb TypeScript rules and Stylelint with sequential script

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true, node: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'function-expression',
+      },
+    ],
+    'react/require-default-props': 'off',
+    'no-param-reassign': [
+      'error',
+      { props: true, ignorePropertyModificationsFor: ['state'] },
+    ],
+    'react/jsx-no-bind': [
+      'error',
+      {
+        allowArrowFunctions: true,
+        allowFunctions: true,
+        allowBind: false,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'import/extensions': 'off',
+  },
+};
